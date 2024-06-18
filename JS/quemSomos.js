494f8599a9449476aa3fb9c9e88f67b946adca9b
Felype <felype.f.sposito@gmail.com>
2024-06-17 21:19:41 -0300

    refazendo a navbar responsiva; Trocando o carrossel da pagina inicial, fazendo a responsividade da pagina inicial na parte quem somos, trocando botao de redirecionamento inicial para a pagina quem somos (sem colocar a ancora)

--- JS/quemSomos.js
+++ JS/quemSomos.js
@@ -1,12 +1,9 @@
 
-
-
-let menuToggle = document.querySelector('.menu-toggle');
-let navbarLinks = document.querySelector('.navbar-links');
+const menuToggle = document.querySelector('.menu-toggle');
+const navbarLinks = document.querySelector('.navbar-links');
 
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active');
   navbarLinks.classList.toggle('active');
 });
-
 
