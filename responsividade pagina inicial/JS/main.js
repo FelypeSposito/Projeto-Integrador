27aeff73ec15b3d86cf58a15b77ef62ade05dd50
Felype <felype.f.sposito@gmail.com>
2024-06-18 17:04:42 -0300

    adicionando responsividade nos cards da landing page

---
+++ responsividade pagina inicial/JS/main.js
@@ -0,0 +1,12 @@
+
+
+
+let menuToggle = document.querySelector('.menu-toggle');
+let navbarLinks = document.querySelector('.navbar-links');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  navbarLinks.classList.toggle('active');
+});
+
+
